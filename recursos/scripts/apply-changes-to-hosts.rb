0bfaad4f879444113c42d5c15dbbd6b33c2f1a38
David Vargas <davidvargas.tenerife@gmail.com>
2016-03-17 12:07:26 +0000

    U install minicom

--- recursos/scripts/apply-changes-to-hosts.rb
+++ recursos/scripts/apply-changes-to-hosts.rb
@@ -15,30 +15,43 @@ Disclaimer  : Sorry about my english! I'm Spanish speaker.
 Remember    :
 * Use `ssh-keygen` on your local PC to generate SSH key pair
 * Use `ssh-copy-id` on your local PC to copy your plubic key into remote host
+
 =end
 
-ROOT_PASSWORD="change-this"
-
-def changes_departamento
-  # Apply this actions to PC's into departamento
-  (1..5).each do |ip|
-    IP="172.31.#{ip.tos}.0"
-    puts "* host[#{IP}]"
-    system("ssh root@#{IP} 'hostname -f'") #show remote hostname
-    system("ssh root@#{IP} \"echo 'root:#{ROOT_PASSWORD}' | chpasswd" # set root password
-  end 
+def departamento
+  equipos=[]
+  (1..5).each { |i| equipos << "172.31.#{i}.0" }
+  return equipos
 end
 
-def changes_aula109() {
-  # Apply this actions to PC's into aula109
-  (1..28).each do |i|
-    IP="172.19.#{i}.0"
-    puts  "* host[$IP]"
-    #ssh root@$IP 'hostname -f' #show remote hostname
-    #ssh root@$IP "echo \"root:$ROOT_PASSWORD\" | chpasswd" # set root password
-    system("ssh root@#{IP} 'mount /dev/sda2 /mnt/entorno2; rm -r /mnt/entorno2/VHDs/*'") #remove VHDs files
-  end
+def aula109
+  equipos=[]
+  (1..28).each { |i| equipos << "172.19.#{i}.0" }
+  return equipos
 end
 
-changes_aula109
+def set_password(ip,password)
+  system("ssh root@#{ip} 'hostname -f'") #show remote hostname
+  system("ssh root@#{ip} \"echo 'root:#{password}' | chpasswd") # set root password
+end
 
+def delete_vhds(ip)
+  system("ssh root@#{ip} 'mount /dev/sda2 /mnt/entorno2; rm -r /mnt/entorno2/VHDs/*'") #remove VHDs files
+end
+
+def instalar_minicom(ip)
+  puts "* host[#{ip}]: Instalando minicom..."
+  system("ssh root@#{ip} 'apt-get install minicom -y --force-yes'")
+  system("ssh root@#{ip} 'adduser asir tty'")
+  system("ssh root@#{ip} 'chmod 660 /dev/tty8'")
+  puts "* host[#{ip}]: Minicom OK!"
+end
+
+equipos=aula109
+
+threads=[]
+equipos.each do |ip| 
+  threads << Thread.new{ instalar_minicom(ip) } 
+end
+threads.each { |t| t.join }
+ 
--- recursos/scripts/apply-changes-to-hosts.sh
+++ recursos/scripts/apply-changes-to-hosts.sh
@@ -30,15 +30,69 @@ function apply_changes_to_departamento() {
 
 function apply_changes_to_aula109() {
 	# Apply this actions to PC's into aula109
-	for i in `seq 1 28`; 
+	for i in `seq 2 28`; 
 	do
     		IP="172.19.$i.0"
     		echo "* host[$IP]"
-    		#ssh root@$IP 'hostname -f' #show remote hostname
-    		#ssh root@$IP "echo \"root:$ROOT_PASSWORD\" | chpasswd" # set root password
-    		ssh root@$IP 'mount /dev/sda2 /mnt/entorno2; rm -r /mnt/entorno2/VHDs/*' #remove VHDs files
+		instalar_minicom
 	done  
 }
 
+function instalar_minicom() {
+  ssh root@$IP 'apt-get install minicom -y --force-yes'
+  ssh root@$IP 'adduser asir tty'
+  ssh root@$IP 'chmod 660 /dev/tty8'
+}
+
+#function cambiar_claves() {
+#    		#ssh root@$IP 'hostname -f' #show remote hostname
+#   		#ssh root@$IP "echo \"root:$ROOT_PASSWORD\" | chpasswd" # set root password
+#    		#ssh root@$IP 'mount /dev/sda2 /mnt/entorno2; rm -r /mnt/entorno2/VHDs/*' #remove VHDs files
+#}
+
 apply_changes_to_aula109
+
+#!/usr/bin/ruby
+# encoding: utf-8
+
+=begin
+Author      : David Vargas
+Date        : 13-16-2016
+Description :
+    This script execute remotely commands on several hosts at a time.
+
+    It's necesary generate on your local PC the SSH key pairs (ssh-keygen), and
+    copy the public key on every remote hosts (ssh-copy-id). So you can execute
+    the script without write the password to access the remote host.
+
+Disclaimer  : Sorry about my english! I'm Spanish speaker.
+Remember    :
+* Use `ssh-keygen` on your local PC to generate SSH key pair
+* Use `ssh-copy-id` on your local PC to copy your plubic key into remote host
+=end
+
+ROOT_PASSWORD="change-this"
+
+def changes_departamento
+  # Apply this actions to PC's into departamento
+  (1..5).each do |ip|
+    IP="172.31.#{ip.tos}.0"
+    puts "* host[#{IP}]"
+    system("ssh root@#{IP} 'hostname -f'") #show remote hostname
+    system("ssh root@#{IP} \"echo 'root:#{ROOT_PASSWORD}' | chpasswd" # set root password
+  end 
+end
+
+def changes_aula109() {
+  # Apply this actions to PC's into aula109
+  (1..28).each do |i|
+    IP="172.19.#{i}.0"
+    puts  "* host[$IP]"
+    #ssh root@$IP 'hostname -f' #show remote hostname
+    #ssh root@$IP "echo \"root:$ROOT_PASSWORD\" | chpasswd" # set root password
+    system("ssh root@#{IP} 'mount /dev/sda2 /mnt/entorno2; rm -r /mnt/entorno2/VHDs/*'") #remove VHDs files
+  end
+end
+
+changes_aula109
 
