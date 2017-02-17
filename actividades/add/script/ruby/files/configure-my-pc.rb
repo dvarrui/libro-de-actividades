8f9359b700c7e2784a0278a37b0e03338856a645
David Vargas <davidvargas.tenerife@gmail.com>
2017-02-17 08:00:46 +0000

    N ruby script demo

---
+++ actividades/add/script/ruby/files/configure-my-pc.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+#
+# URL sed:
+# http://www.sysadmit.com/2015/07/linux-reemplazar-texto-en-archivos-con-sed.html
+
+# Esquema:
+# * hostname y /etc/hosts
+# * interfaces de red
+# * DNS, gateway
+# * Usuario, SSHServer, PermitRootLogin Yes
+
