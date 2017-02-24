9291cb4ef73dd326c09c4dcf116217a7a2e343ee
David Vargas <davidvargas.tenerife@gmail.com>
2017-02-24 13:34:34 +0000

    N ruby scripts

--- actividades/add/script/ruby/files/configurar1.rb
+++ actividades/add/script/ruby/files/configurar1.rb
@@ -1,13 +1,10 @@
 #!/usr/bin/ruby
-#
-# URL sed:
-# http://www.sysadmit.com/2015/07/linux-reemplazar-texto-en-archivos-con-sed.html
-#
+
 # Esquema:
-# * hostname y /etc/hosts
+# * hostname, etc host
 # * interfaces de red
-# * DNS, gateway
-# * Usuario, SSHServer, PermitRootLogin Yes
+# * dns gateway
+# * usuario, opensshhserver permitrootlogin yes
 
 require 'progress_bar'
 
