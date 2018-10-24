e43873870cf7a1ca93c2a3c39d992ef6b57c2150
David <dvarrui@gmail.com>
2018-10-24 17:59:18 +0100

    N simulate typing

---
+++ actividades/add/script/ruby/files/simulate-typing.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+# Simulate typing
+# https://www.linuxjournal.com/content/simulate-typing-c-program
+
+def print_with_delay(text, seconds)
+  text.each_char do |c|
+    sleep(seconds)
+    print(c)
+  end
+end
+
+def execute_this(command)
+  output = ''
+  begin
+    output = `#{command}`
+  rescue Exception => e
+    puts "[ERROR] comando incorrecto: #{e}"
+  end
+  output
+end
+
+file = 'simulate-typing.txt'
+content = `cat #{file}`
+lines = content.split("\n")
+#lines = ['pwd','whoami','ls']
+prompt = "simulating> "
+
+lines.each do |command|
+  sleep(0.2)
+  print prompt
+  sleep(0.2)
+  print_with_delay(command,0.4)
+  print "\n"
+  sleep(0.3)
+  output = execute_this command
+  print_with_delay(output,0.04)
+end
