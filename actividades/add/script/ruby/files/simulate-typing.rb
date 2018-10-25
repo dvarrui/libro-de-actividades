e5bdc0600e983c59fcffe74d87894ad63b6c818c
David <dvarrui@gmail.com>
2018-10-25 08:01:23 +0100

    U

--- actividades/add/script/ruby/files/simulate-typing.rb
+++ actividades/add/script/ruby/files/simulate-typing.rb
@@ -1,44 +1,54 @@
 #!/usr/bin/ruby
 # encoding: utf-8
+#
 # Simulate typing
-# https://www.linuxjournal.com/content/simulate-typing-c-program
+# * https://www.linuxjournal.com/content/simulate-typing-c-program
 
 require 'rainbow'
 
-def print_with_delay(text, seconds)
-  text.each_char do |c|
-    sleep(seconds)
-#    print(Rainbow.(c.to_s).color(:blue))
-    print(c)
+class SimulateTyping
+
+  def initialize(filename = 'simulate-typing.txt')
+    @filename = filename
+    content = `cat #{@filename}`
+    @lines = content.split("\n")
+    #lines = ['pwd','whoami','ls']
+    @prompt = "simulating> "
   end
-end
 
-def execute_this(command)
-  output = ''
-  begin
-    output = `#{command}`
-  rescue Exception => e
-    puts "[ERROR] comando incorrecto: #{e}"
+  def play
+    @lines.each do |command|
+      sleep(0.2)
+      print @prompt
+      sleep(0.2)
+      print_with_delay(command,0.4)
+      #  gets
+      print "\n"
+      sleep(0.3)
+      output = execute_this command
+      print_with_delay(output,0.04)
+    end
+    puts "Bye-bye! Simulation end!"
   end
-  output
+
+  def print_with_delay(text, seconds)
+    text.each_char do |c|
+      sleep(seconds)
+  #    print(Rainbow.(c.to_s).color(:blue))
+      print(c)
+    end
+  end
+
+  def execute_this(command)
+    output = ''
+    begin
+      output = `#{command}`
+    rescue Exception => e
+      puts "[ERROR] comando incorrecto: #{e}"
+    end
+    output
+  end
+
 end
 
-file = 'simulate-typing.txt'
-content = `cat #{file}`
-lines = content.split("\n")
-#lines = ['pwd','whoami','ls']
-prompt = "simulating> "
-
-lines.each do |command|
-  sleep(0.2)
-  print prompt
-  sleep(0.2)
-  print_with_delay(command,0.4)
-#  gets
-  print "\n"
-  sleep(0.3)
-  output = execute_this command
-  print_with_delay(output,0.04)
-end
-
-puts "Simulation end!"
+SimulateTyping.new.play
