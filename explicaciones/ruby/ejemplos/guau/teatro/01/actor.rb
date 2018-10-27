e84b7aaf4f1b5f347ea355529205fbca2af052f2
David <dvarrui@gmail.com>
2018-10-27 07:17:57 +0100

    N

---
+++ explicaciones/ruby/ejemplos/guau/teatro/01/actor.rb
@@ -0,0 +1,26 @@
+
+class Actor
+  def initialize(name)
+    @name = name
+  end
+
+  def dice(texto)
+    info = @name
+    frases = texto.split('.')
+    # Decir la primera frase
+    puts info + ' : ' + frases[0].strip + '.'
+    frases.delete_at 0
+    info = ' '* @name.size
+    frases.each do |frase|
+      puts info + ' : ' + frase.strip + '.'
+    end
+  end
+
+  def dice_tranquilo(texto)
+    dice texto
+  end
+
+  def dice_gritando(texto)
+    dice texto.upcase
+  end
+end
