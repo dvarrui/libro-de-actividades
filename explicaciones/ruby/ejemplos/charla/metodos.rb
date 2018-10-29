d8ab0d1e3bf13d28ff13a3d9c499dbf8ab543fc5
David <dvarrui@gmail.com>
2018-10-29 00:03:32 +0000

    U

---
+++ explicaciones/ruby/ejemplos/charla/metodos.rb
@@ -0,0 +1,24 @@
+
+class Persona
+  def initialize(name)
+    @name = name
+  end
+
+  def me_presento
+    puts "Hola soy #{@name}!"
+  end
+
+  def insultar
+    puts "Hola soy un tonto!"
+  end
+
+  def insultar!
+    @name = "tonto"
+    me_presento
+  end
+
+  def eres_tonto?
+    return true if @name == "tonto"
+    false
+  end
+end
