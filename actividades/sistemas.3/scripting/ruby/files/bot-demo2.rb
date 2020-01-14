71e954c85d9ac514278cef06a43ac49add626b1c
David <dvarrui@protonmail.com>
2020-01-14 08:48:59 +0000

    N v2

--- actividades/sistemas.3/scripting/ruby/files/bot-demo2.rb
+++ actividades/sistemas.3/scripting/ruby/files/bot-demo2.rb
@@ -13,10 +13,12 @@ puts "[INFO] Running bot #{$0}..."
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     puts " => #{message.text}"
+    hello = Regexp.new("/hello|hello#{bot_username}")
+    byebye = Regexp.new("/byebye|byebye#{bot_username}")
     case message.text
-    when "/hello"
+    when hello
       bot.api.send_message(chat_id: message.chat.id, text: "Hello Word!")
-    when "/byebye"
+    when byebye
       bot.api.send_message(chat_id: message.chat.id, text: "Bye bye!")
       exit
     end
