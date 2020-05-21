31317768c39f4a86098bfdf8bb69c88bafc5877f
dvarrui <dvarrui@protonmail.com>
2020-05-21 11:50:03 +0100

    U

--- actividades/sistemas.2/comandos/files/backup-windows.rb
+++ actividades/sistemas.2/comandos/files/backup-windows.rb
@@ -1,40 +1,19 @@
 
 require 'fileutils'
 
-
-
 # Crea el directorio backup si no existe
-
 FileUtils.mkdir('c:\backup') unless File.exist?('c:\backup')
 
-
 # Lee los nombres de los archivos que buscamos
-
-nombres = Dir.glob(File.join('c:', '**', '*.exe')
-) + 
-          Dir.glob(File.join('c:', '**', '*.txt'))
- + 
-          Dir.glob(File.join('c:', '**', '*.rb'))
-
-
+nombres = Dir.glob(File.join('c:', '**', '*.png')) + 
+          Dir.glob(File.join('c:', '**', '*.jpg')) + 
+	  Dir.glob(File.join('c:', '**', '*.jpeg'))
 
 # Para cada nombre de archivo hacemos lo siguiente:
-
-
 nombres.each do |i|
-  
-  # Nombre del fichero destino
-  
-  dest = File.join('C:','backup', File.basename(i))
-  
-  # Copia source en dest
-  
-  FileUtils.cp(i, dest)      
-  print '.'                  
-  # Muestra un punto en pantalla
-
+  dest = File.join('C:', 'backup', File.basename(i)) # Nombre del fichero destino
+  FileUtils.cp(i, dest)    # Copia source en dest
+  print '.'                # Muestra un punto en pantalla
 end
 
-
-
-puts "Fin del script."
+puts "Fin del script!"
