ce6dfcbf782869a2b775575b1725a6cea4df420e
David Vargas <dvarrui@protonmail.com>
2022-01-19 12:19:20 +0000

    U

--- actividades/global/vagrant/opensuse/install-software.sh
+++ actividades/global/vagrant/opensuse/install-software.sh
@@ -2,8 +2,18 @@
 
 zypper refresh
 
-zypper in -y vim tree nmap
-zypper in -y neofetch lsb-release
+zypper install -y vim tree nmap git
+zypper install -y neofetch lsb-release
+
+zypper install -y figlet
+figlet OpenSUSE > /etc/motd
+echo "" >> /etc/motd
+echo "David Vargas Ruiz" >> /etc/motd
+echo "https://github.com/dvarrui" >> /etc/motd
+
+echo "# Adding more alias" >> /home/vagrant/.bashrc
+echo "alias c='clear'" >> /home/vagrant/.bashrc
+echo "alias v='vdir'" >> /home/vagrant/.bashrc
 
 lsb_release -d
 
