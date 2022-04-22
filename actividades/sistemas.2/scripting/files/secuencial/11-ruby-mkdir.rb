c6f9911a5e08cb8beddbef1367e9f55a35477de2
David Vargas <dvarrui@protonmail.com>
2022-04-22 10:21:52 +0100

    [F] .

--- actividades/sistemas.2/scripting/files/secuencial/11-ruby-mkdir.rb
+++ actividades/sistemas.2/scripting/files/secuencial/11-ruby-mkdir.rb
@@ -7,8 +7,8 @@ echo "[+] Creando el fichero README"
 system "touch backupXXr/README.txt"
 
 puts "[+] Poniendo contenido al fichero README"
-echo "Nombre del responsable: OBIWAN" >> backupXXr/README.txt
-echo "Este es el contenido del fichero README.txt" >> backupXXr/README.txt
+system("echo 'Nombre del responsable: OBIWAN' >> backupXXr/README.txt")
+system("echo 'Este es el contenido del fichero README.txt' >> backupXXr/README.txt")
 
 echo "[+] Fin del script #{$0}"
 exit 0
