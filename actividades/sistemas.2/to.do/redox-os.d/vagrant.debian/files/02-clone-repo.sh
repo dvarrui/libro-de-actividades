4544eda05f37a1c6059b11040e5085c52dd369ea
David Vargas <dvarrui@protonmail.com>
2024-04-02 12:23:16 +0100

    [D]

--- actividades/sistemas.2/to.do/redox-os.d/vagrant.debian/files/02-clone-repo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p tryredox
-cd tryredox
-git clone https://gitlab.redox-os.org/redox-os/redox.git --origin upstream --recursive
-cd redox
-git submodule update --recursive --init
-
